cfafcae08aee8d7277e0aef685daefabd68194e8
Elmala02 <malaversierrad@gmail.com>
2026-08-19 16:52:08 -0500

    feat: agregar mapeo de replicacion origen -1005492244914 a destino -1005302047567

--- config.py
+++ config.py
@@ -36,6 +36,9 @@ REPLICATION_MAP = {
     ],
     "@aura_capital1": [
         {"dest": -1005386250677, "name": "Aura Capital", "priority": 1, "allow_media": True}
+    ],
+    -1005492244914: [
+        {"dest": -1005302047567, "name": "Grupo 5492244914", "priority": 1, "allow_media": True}
     ]
 }
 
